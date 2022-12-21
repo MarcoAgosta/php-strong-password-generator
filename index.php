f6365bb8476347634d59c867237c7c01909f60ad
MarcoAgosta <agostamarco@libero.it>
2022-12-21 15:38:10 +0100

    Iniziata creazione della pagina index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong-password generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="CSS/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="bg-blue">
+        <div class="container">
+            <div class="text-center pt-5">
+                <h1 class="gray mb-4">Strong Password Generator</h1>
+                <h2 class="white">Genera una password sicura</h2>
+            </div>
+
+            <form action="">
+                
+            </form>
+        </div>
+    </div>
+</body>
+</html>
