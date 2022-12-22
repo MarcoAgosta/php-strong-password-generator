b894ef6845403d887acce4831533577dd9c53fc4
MarcoAgosta <agostamarco@libero.it>
2022-12-22 09:44:04 +0100

    Aggiunti anche i numeri

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
     function creaPassword ($num){
         $password="";
         for ($i=1; $i<=$num; $i++){
-            $password .= chr(rand(65, 122));
+            $password .= chr(rand(48, 122));
             echo "<script>console.log('{$password}')</script>";
         };
         return $password;
